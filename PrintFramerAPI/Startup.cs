3d76b1f16f8833abb90747262fbfd22caa7f5aae
Kayê Holanda <kaye.holanda@edge.ufal.br>
2024-07-05 16:01:57 -0300

    update documentation

--- PrintFramerAPI/Startup.cs
+++ PrintFramerAPI/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace PrintFramerAPI
 {
@@ -26,6 +29,28 @@ namespace PrintFramerAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                {
+                    Version = "v1",
+                    Title = "PrintFramer API",
+                    Description = "Calculates the cost of a picture frame based on its dimensions.",
+                    TermsOfService = new Uri("https://go.microsoft.com/fwlink/?LinkID=206977"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Your name",
+                        Email = string.Empty,
+                        Url = new Uri("https://learn.microsoft.com/training")
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +58,12 @@ namespace PrintFramerAPI
         {
             if (env.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
+
                 app.UseDeveloperExceptionPage();
             }
 
